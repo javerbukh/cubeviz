15140a335bf40badcb7e079320a9a56f427e0f97
Jesse Averbukh <javerbukh@stsci.edu>
2018-03-09 12:23:55 -0500

    debugging

--- cubeviz/data_factories/__init__.py
+++ cubeviz/data_factories/__init__.py
@@ -102,13 +102,14 @@ class DataConfiguration:
         :param data_filename:
         :return:
         """
-
+        print("in load data")
         label = None
         data = None
 
         for data_filename in data_filenames.split(','):
 
             hdulist = fits.open(data_filename)
+            print(hdulist)
 
             if not label:
                 label = "{}: {}".format(self._name, splitext(basename(data_filename))[0])
@@ -363,6 +364,7 @@ class DataFactoryConfiguration:
 
         # Remove all pre-defined data configuration loaders in Glue. Then, if a user tries to open an IFU FITS
         # file that is not known to us a popup will come up saying cubeviz does not recognize the data format.
+        print("in init")
         if remove_defaults:
             data_factory._members = []
 
@@ -398,6 +400,7 @@ class DataFactoryConfiguration:
 
             try:
                 priority = int(cfg.get('priority', 0))
+                print("in try", priority, cfg)
             except:
                 priority = 0
 
